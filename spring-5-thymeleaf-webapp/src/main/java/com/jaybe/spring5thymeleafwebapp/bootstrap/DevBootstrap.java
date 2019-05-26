a05aa3d6be232a16ebf17edfe83fb4cdff312e9c
Jabels <jabels1121@gmail.com>
2019-05-26 22:22:19 +0300

    Adding Publisher entity, update Book object to use publisher and create publisherRepository

--- spring-5-thymeleaf-webapp/src/main/java/com/jaybe/spring5thymeleafwebapp/bootstrap/DevBootstrap.java
+++ spring-5-thymeleaf-webapp/src/main/java/com/jaybe/spring5thymeleafwebapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package com.jaybe.spring5thymeleafwebapp.bootstrap;
 
 import com.jaybe.spring5thymeleafwebapp.entity.Author;
 import com.jaybe.spring5thymeleafwebapp.entity.Book;
+import com.jaybe.spring5thymeleafwebapp.entity.Publisher;
 import com.jaybe.spring5thymeleafwebapp.repositories.AuthorRepository;
 import com.jaybe.spring5thymeleafwebapp.repositories.BookRepository;
+import com.jaybe.spring5thymeleafwebapp.repositories.PublisherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -16,11 +18,13 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private BookRepository bookRepository;
     private AuthorRepository authorRepository;
+    private PublisherRepository publisherRepository;
 
     @Autowired
-    public DevBootstrap(BookRepository bookRepository, AuthorRepository authorRepository) {
+    public DevBootstrap(BookRepository bookRepository, AuthorRepository authorRepository, PublisherRepository publisherRepository) {
         this.bookRepository = bookRepository;
         this.authorRepository = authorRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -29,14 +33,18 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     }
 
     private void initData() {
+        Publisher publisher = new Publisher("Harper Collins", "Some city, some street");
+
+        publisherRepository.save(publisher);
+
 
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        Book ddd = new Book("Domain Driven Design", "1234", publisher);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "23444", "Worx");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", publisher);
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
--- spring-5-thymeleaf-webapp/src/main/java/com/jaybe/spring5thymeleafwebapp/entity/Book.java
+++ spring-5-thymeleaf-webapp/src/main/java/com/jaybe/spring5thymeleafwebapp/entity/Book.java
@@ -22,7 +22,8 @@ public class Book {
 
     private String isbn;
 
-    private String publisher;
+    @OneToOne
+    private Publisher publisher;
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
@@ -30,13 +31,13 @@ public class Book {
     private Set<Author> authors = new HashSet<>();
 
 
-    public Book(String title, String isbn, String publisher) {
+    public Book(String title, String isbn, Publisher publisher) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
     }
 
-    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
