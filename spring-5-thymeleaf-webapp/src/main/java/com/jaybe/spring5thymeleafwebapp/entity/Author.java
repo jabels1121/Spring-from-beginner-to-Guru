a1a5869d1643a4eb7e52f83e552fc27b1c5f35fd
Jabels <jabels1121@gmail.com>
2019-05-26 20:31:17 +0300

    adding JPA entities

---
+++ spring-5-thymeleaf-webapp/src/main/java/com/jaybe/spring5thymeleafwebapp/entity/Author.java
@@ -0,0 +1,36 @@
+package com.jaybe.spring5thymeleafwebapp.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books = new HashSet<>();
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Author(String firstName, String lastName, Set<Book> books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+}
